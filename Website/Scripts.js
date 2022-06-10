2165a22cb3712fc9d8634e4dc360759ea346bee7
TobleroneBill <101929340+TobleroneBill@users.noreply.github.com>
2022-06-10 15:36:25 +0100

    basic key checking functions added + comments

--- Website/Scripts.js
+++ Website/Scripts.js
@@ -1,3 +1,4 @@
+//json =3
 const fields = {  
 name:document.getElementById("name") ,
 email:document.getElementById("email"),
@@ -5,21 +6,24 @@ card:document.getElementById("card"),
 submit:document.getElementById("submitButton"),
 }
 
+//adds Event listeners. doesnt check for input on submit button.
 Object.keys(fields).forEach(key => {
-fields[key].addEventListener("click",myFunction);
 if(key != "submit"){
-  fields[key].addEventListener('input',logText);
+  fields[key].addEventListener('input',LogText);
+}else{
+  fields[key].addEventListener("click",submit);
 }
 });
 
-
-
-
-function myFunction() {
-  console.log("Hello World");
+function submit(){
+console.log('Pressed Submit');
 }
 
-function logText(e){
-  const field = e;
-  console.log(field.data);
+//console logs text from 3 fields.
+function LogText(e){
+  Object.keys(fields).forEach(key => {
+    if(key != "submit"){
+      console.log(fields[key].name+": "+fields[key].value);
+    }
+  });
 }
