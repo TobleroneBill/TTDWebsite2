35012b8ec2bebbf1430fe780dad1a81e54155512
TobleroneBill <101929340+TobleroneBill@users.noreply.github.com>
2022-06-10 12:49:45 +0100

    Added event listeners to all fields

--- Website/Scripts.js
+++ Website/Scripts.js
@@ -1,1 +1,14 @@
-console.log("Hello!");
+const userElements = [document.getElementById("name"),document.getElementById("email"),document.getElementById("card"),document.getElementById("submitButton")]
+
+userElements.forEach(element => {
+    element.addEventListener("click", myFunction);
+});
+
+
+
+
+
+
+function myFunction() {
+  console.log("Hello World");
+}
