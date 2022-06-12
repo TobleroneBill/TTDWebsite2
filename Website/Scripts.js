9cb28552953b8d725b2d8000c77fb2177d93b38e
TobleroneBill <101929340+TobleroneBill@users.noreply.github.com>
2022-06-12 10:12:38 +0100

    Removed commented code

--- Website/Scripts.js
+++ Website/Scripts.js
@@ -52,27 +52,12 @@ card:new InputField('card',document.getElementById("card")),
 submit:document.getElementById("submitButton"),
 }
 
-console.log(fields[name]);
 
 Object.keys(fields).forEach(key => {
   if(key != "submit"){
     fields[key].LogSelf();
-    //fields[key]._type.addEventListener('input', fields[key].CheckAscii);
-  }
+  } 
 });
-
-
-//adds Event listeners. Doesn't check for input on submit button only click.
-/*
-Object.keys(fields).forEach(key => {
-if(key != "submit"){
-  fields[key].addEventListener('input',LogText);
-}else{
-  fields[key].addEventListener("click",submit);
-}
-});
-*/
-
 
 //submit event 
 //TODO: open mail HTML form action when all info is valid
@@ -80,32 +65,3 @@ function submit(){
 console.log('Pressed Submit');
 }
 
-//console logs text from 3 fields.
-/*
-function LogText(e){
-  Object.keys(fields).forEach(key => {
-    var value = fields[key].value
-    if(key != "submit"){
-      console.log(fields[key].name+": "+fields[key].value);
-    }
-
-    //for loop to check each keycode
-    for(var i=0; i < value.length;i++){
-        //console.log(value.charCodeAt(i));     //used to debug keycode values
-
-      //if keycodes are in accepted range
-      if(value.charCodeAt(i) > 31 && value.charCodeAt(i) < 127){
-        //check if the letter at index i has the same keycode as the banned keycodes.
-        invalidkeyCodes.forEach(element => {
-          //if is does match a banned keycode, return false
-          if(value.charCodeAt(i) == element){
-            console.log("banned keycode" + element);
-          }
-        });
-      } else{
-        console.log("input was out of accepted readable char range: " + value.charCodeAt(i));
-      }
-    }
-  });
-}
-*/
