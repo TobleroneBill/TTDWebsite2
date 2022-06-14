6cef0699d420dec6dbe6c7d0ad64d4f83a1c441f
TobleroneBill <101929340+TobleroneBill@users.noreply.github.com>
2022-06-14 15:35:23 +0100

    Zip + project flies of Visual Studio Project
    Uploaded the main folders of the project + wwwroot
    zipped the whole project if you want to download it but you could probably make it yourself lmao

---
+++ Other stuff/Controllers/UserInput.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+namespace MVCPresetCheckout.Controllers
+{
+    public class UserInput : Controller
+    {
+        public IActionResult Index() {
+            return View();
+        }
+
+        // this adds /UserInput/Weclome to the HTTP
+        public IActionResult Welcome(string Message, int NumTimes = 1){
+            ViewData["Message"] = "Hello " + Message;
+            ViewData["NumTimes"] = NumTimes;
+
+            return View();
+        }
+        //bascially these get loaded when appended to the HTTP address. to test copy and paste these on the end of the adressbar (will load the string on black BG)
+    }
+}
